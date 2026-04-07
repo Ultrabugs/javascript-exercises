9d9fc5b43c573a548c97cb0e62cf1c9bb9411731
Ultrabugs <234222906+Ultrabugs@users.noreply.github.com>
2026-04-07 21:52:29 +0100

    Stuck on task 3

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,24 @@
-const palindromes = function () {
-
+const palindromes = function (input) {
+    
+    let hasPunctuation = false;
+    let ConvertInputToArray = [...input];
+    let ReversedInputArray = ConvertInputToArray.sort((a, b) => b - a ? 1 : -1);
+    
+    if(ReversedInputArray.includes("!")) {
+        hasPunctuation = true;
+    }
+     
+    let revertedBackToString = ReversedInputArray.reduce((accumulator, currentValue) => {
+        return accumulator + currentValue;
+    });
+    
+    if(hasPunctuation === true) {
+        let RemovedPunctuationString = revertedBackToString.slice(1);
+        return RemovedPunctuationString + '!' === input ? true : false;
+    } else {
+        return revertedBackToString === input ? true : false 
+    }
+    
 };
 
 // Do not edit below this line
