2a001e3d602e915e5c5c12319454ceecdaed9b85
Ultrabugs <234222906+Ultrabugs@users.noreply.github.com>
2026-04-09 02:22:34 +0100

    My Solution for Palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,25 +1,102 @@
 const palindromes = function (input) {
     
-    let hasPunctuation = false;
-    let ConvertInputToArray = [...input];
-    let ReversedInputArray = ConvertInputToArray.sort((a, b) => b - a ? 1 : -1);
+    // convert input to array lower cased
+    const cleanedString = input.split(" ").join("").toLowerCase();
+    const inputString = cleanedString.split(",").join("");
+    const inputArray = Array.from(inputString.toLowerCase().trim());
+    const nonReversedInputString = inputArray.join("");
     
-    if(ReversedInputArray.includes("!")) {
-        hasPunctuation = true;
+    if(inputArray.includes("!")){
+        let locationToSlice = inputArray.indexOf("!");
+        inputArray.splice(locationToSlice, 1)
+       
     }
-     
-    let revertedBackToString = ReversedInputArray.reduce((accumulator, currentValue) => {
-        return accumulator + currentValue;
-    });
     
-    if(hasPunctuation === true) {
-        let RemovedPunctuationString = revertedBackToString.slice(1);
-        return RemovedPunctuationString + '!' === input ? true : false;
-    } else {
-        return revertedBackToString === input ? true : false 
+    if(inputArray.includes(".")) {
+        let locationToSlice = inputArray.indexOf(".");
+        inputArray.splice(locationToSlice, 1);
     }
+    
+    const regularInput = inputArray.join("");
+    const reversedInput = inputArray.reverse();
+   
+    const reversedInputToString = reversedInput.join("");
+    
+    console.log(regularInput)
+    console.log(reversedInputToString)
+    
+    return regularInput === reversedInputToString ? true : false;
     
 };
+ 
+ 
+ 
+ 
+ 
 
 // Do not edit below this line
 module.exports = palindromes;
+
+
+// const palindromes = function (input) {
+    
+    
+//     const inputArray = Array.from(input);
+    
+//     if(inputArray.includes("!")){
+//         let locationToSlice = inputArray.indexOf("!");
+//         inputArray.splice(locationToSlice, 1)
+       
+//     }
+    
+//     console.log(inputArray);
+    
+//     const reversedInput = inputArray.reverse();
+    
+//     console.log(reversedInput); 
+    
+//     const reversedInputToString = reversedInput.join("");
+
+//     const nonReversedInputString = inputArray.join("");
+    
+//     console.log(reversedInputToString)
+//     console.log(nonReversedInputString)
+    
+    
+//     return reversedInputToString === nonReversedInputString ? console.log("true") : console.log("false")
+// };
+ 
+// palindromes("race");
+
+
+
+// const palindromes = function (input) {
+    
+    
+//     const inputArray = Array.from(input.toLowerCase());
+//     const nonReversedInputString = inputArray.join("");
+    
+//     if(inputArray.includes("!")){
+//         let locationToSlice = inputArray.indexOf("!");
+//         inputArray.splice(locationToSlice, 1)
+       
+//     }
+    
+//     console.log(inputArray);
+    
+//     const reversedInput = inputArray.reverse();
+    
+//     console.log(reversedInput); 
+    
+//     const reversedInputToString = reversedInput.join("");
+
+    
+    
+//     console.log(reversedInputToString)
+//     console.log(nonReversedInputString)
+    
+    
+//     return reversedInputToString === nonReversedInputString ? console.log("true") : console.log("false")
+// };
+ 
+// palindromes("Animal loots foliated detail of stool lamina.");
