5c49681f34f4c7fd34c9b57f1d4d0f871b2b3699
Ultrabugs <234222906+Ultrabugs@users.noreply.github.com>
2026-04-18 21:18:25 +0100

    Completed solution for find the oldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,30 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    
+    // let people = [];
+    // arr.map((item) => {
+    //     people.push(item.name);
+    //     people.push(item.yearOfDeath - item.yearOfBirth)
+    // });
+    currentYear = (new Date()).getFullYear();
+    people.map((item) => {
+        if(item.yearOfDeath === undefined) {
+            item.yearOfDeath = currentYear;
+            people.push(currentYear);
+        }
+    })
 
+    people.sort((personOne, personTwo) => {
+        personOneAge = personOne.yearOfDeath - personOne.yearOfBirth;
+        personTwoAge = personTwo.yearOfDeath - personTwo.yearOfBirth;
+
+        return personOneAge > personTwoAge ? -1 : 1;
+    });
+
+     
+    let oldest = people[0];
+    return oldest;
+     
+    
 };
 
 // Do not edit below this line
