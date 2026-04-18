08e6a59cc6c5b96c1f7d08ae9a6880f34b39d228
Ultrabugs <234222906+Ultrabugs@users.noreply.github.com>
2026-04-18 05:15:16 +0100

    Solved fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,32 @@
-const fibonacci = function() {
+ const fibonacci = function(number) {
 
-};
+    if(number < 0) {
+        return 'OOPS';
+    }
+
+    let secondLastNumber = 1;
+    let lastNumber = 1;
+    let storedNumber;
+    
+    let numberArray = [0];
+    numberArray.push(secondLastNumber);
+    numberArray.push(lastNumber);
+    
+    for(let x = 0; x <= number; x++){
+        
+        storedNumber = secondLastNumber + lastNumber;
+
+        secondLastNumber = lastNumber;
+
+        lastNumber = storedNumber;
+        numberArray.push(lastNumber);
+         
+    }
+
+    return numberArray[number];
+}
+
+fibonacci();
 
 // Do not edit below this line
 module.exports = fibonacci;
